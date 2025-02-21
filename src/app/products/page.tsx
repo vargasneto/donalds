2bb55d7e06bfa00285fe9e237890c6b473464a05
Pedro <pedrohjs_vargas@hotmail.com>
2025-02-21 19:49:50 -0300

    feat: shadcn e seus componentes iniciais add

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Products = () => {
+  return (
+    <>
+      <div className="p-2 m-7 border border-pink-500 rounded-xl ">
+        <Button> Benas</Button>
+      </div>
+    </>
+  );
+};
+
+export default Products;
