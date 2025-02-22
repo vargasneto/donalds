929b1c7dc1e29310c13c50c100d1ab2b839a79ad
Pedro <pedrohjs_vargas@hotmail.com>
2025-02-21 21:21:56 -0300

    chore: plugins do prettier e eslint adicionados

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,11 +3,13 @@ import { Button } from "@/components/ui/button";
 const Products = () => {
   return (
     <>
-      <div className="p-2 m-7 border border-pink-500 rounded-xl ">
+      <div className="p-2 m-7 border border-pink-500 rounded-xl">
+        <h1 className=" bg-inherit mx-px l"></h1>
         <Button> Benas</Button>
       </div>
     </>
   );
+  
 };
 
 export default Products;
