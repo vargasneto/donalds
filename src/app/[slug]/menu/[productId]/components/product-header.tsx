4334e7c3d880a63a9dc5f55e99fe05c5f09c6c59
Pedro <pedrohjs_vargas@hotmail.com>
2025-02-22 22:02:31 -0300

    feat: componente do header/produtos add

---
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Product } from "@prisma/client";
+import { ChevronsLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+interface ProductHeaderProps{
+    product:Pick<Product,"name"|"imageUrl">
+}
+
+const ProductHeader = ({product}:ProductHeaderProps) => {
+    const router =useRouter()
+    const handleBackClick= () =>router.back();
+    return ( <div className="relative w-full h-[300px]">
+        <Button className="absolute left-4 top-4 rounded-full z-50" variant={"secondary"} size="icon" onClick={handleBackClick}>
+            <ChevronsLeftIcon/>
+                </Button>    
+    
+            <Button className="absolute right-4 top-4 rounded-full z-50" variant={"secondary"} size="icon">
+            <ScrollTextIcon/>
+                </Button>
+                
+            <Image className="object-contain" src={product.imageUrl} alt={product.name} fill/>
+        </div> );
+}
+ 
+export default ProductHeader;
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,16 +1,18 @@
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 interface ProductsProps{
     products:Product[]
 }
 
 const Products = ({products}: ProductsProps) => {
+    const{slug}=useParams<{slug:string}>();
     return ( <div className="space-y-3 px-5">
 {products.map(product=>(
  
- <Link className="flex items-center justify-between gap-10 py-5 border-b" key={product.id} href="/">
+<Link className="flex items-center justify-between gap-10 py-5 border-b" key={product.id} href= {`/${slug}/menu/${product.id}`}>
     {/* left */}
     <div>
         <h3 className="text-sm font-medium">{product.name}</h3>
