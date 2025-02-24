49c56e28ad6fb649ada7d2bae10383cc30f1b419
Pedro <pedrohjs_vargas@hotmail.com>
2025-02-24 00:25:34 -0300

    feat:cart product agora pode ser decrementado e incrementado

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -12,7 +12,7 @@ interface CartItemProps{
 }
 
 const CartProductItem = ({product}:CartItemProps) => {
-    const{decreaseProductQuantity}=useContext(CartContext)
+    const{decreaseProductQuantity,increaseProductQuantity}=useContext(CartContext)
     return (
         <div className="flex items-center justify-between">
             <div className="flex items-center gap-3">
@@ -30,7 +30,7 @@ const CartProductItem = ({product}:CartItemProps) => {
         <ChevronLeftIcon />
         </Button>
 <p className="text-xs w-7">{product.quantity}</p>
-        <Button className="h-7 w-7 rounded-lg" variant='destructive'>
+        <Button className="h-7 w-7 rounded-lg" variant='destructive' onClick={()=>increaseProductQuantity(product.id)}>
         <ChevronRightIcon />
         </Button>
             </div>
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -11,13 +11,15 @@ export interface IcartContext{
     toggleCart: ( )=> void;
     addProduct: (Product:CartProduct) => void
     decreaseProductQuantity:(productId:string) =>void
+    increaseProductQuantity:(productId:string) =>void
 }
 
 export const CartContext = createContext<IcartContext>({isOpen:false,
     products:[],
     toggleCart:()=> {},
 addProduct:()=> {},
-decreaseProductQuantity:()=>{}
+decreaseProductQuantity:()=>{},
+increaseProductQuantity:()=>{}
 })
 
 export const CartProvider = ({children}:{children:ReactNode})=>{
@@ -64,12 +66,25 @@ export const CartProvider = ({children}:{children:ReactNode})=>{
         })
 
     }
+
+    const increaseProductQuantity = (productId:string) => {
+        setProducts(prevProducts=>{
+            return prevProducts.map(prevProduct=>{
+                if(prevProduct.id != productId)
+                {
+                    return prevProduct
+                }
+                    return {...prevProduct,quantity:prevProduct.quantity + 1}
+                        })
+        })
+
+    }
     return(<CartContext.Provider value={{
         isOpen,
         products,
         toggleCart,
         addProduct,
-        decreaseProductQuantity
+        decreaseProductQuantity,increaseProductQuantity
     }} >
         {children}
     </CartContext.Provider> 
